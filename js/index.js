874d27f4f12bdb1492333af838b02a84798e0d92
max davydenko <maksim.davydenko@gmail.com>
2020-05-07 00:02:21 +0300

    add muilty fetch

--- js/index.js
+++ js/index.js
@@ -10,37 +10,79 @@ const dataSource = [];
 
 let startPost = 0;
 let endPost = 6;
+const filterList = ['Ordinary Drink', 'Cocktail','Milk / Float / Shake','Other/Unknown','Cocoa','Shot','Coffee / Tea','Homemade Liqueur','Beer'];
 
 function getCocktails() {
-     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink')
-    .then(
-        function(response){
-        if(response.status !== 200){
-            console.log(`server has some problem ${response.status}`);
-            return;
-        }
-        response.json().then(function(data){
-            displayCocktails(data.drinks);
-            (data.drinks).forEach(cocktail =>{
-                dataSource.push(cocktail);
-            })
-        });
-    }
-)
-    .catch(function(err){
-        console.log(`fetch err ${err}`);
+
+    const url ='https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
+    const filterArr = filterList.map(item => url + item);
+
+    // filterArr.forEach(item => {
+    //     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filterList[4]}`)
+    // Promise.all(filterArr.map(u=>fetch(u))).then(responses =>
+    //     Promise.all(responses.map(res => res.text()))
+    // )
+    // .then(
+    //         function (response) {
+    //             if (response.status !== 200) {
+    //                 console.log(`server has some problem ${response.status}`);
+    //                 return;
+    //             }
+    //             response.json().then(function (data) {
+    //                 (data.drinks).forEach(cocktail => {
+    //                     dataSource.push(cocktail);
+    //                 })
+    //             });
+    //         }
+    //     )
+    //     .catch(function (err) {
+    //         console.log(`fetch err ${err}`);
+    //     })
+
+    // Promise.all(filterArr.map(url =>
+    //     fetch(url).then(resp => resp.json())
+    // )).then(texts => {
+    //     texts.forEach(items => {
+    //        //  (items.drinks).forEach(item => {
+    //        //     dataSource.push(item);
+    //        // })
+    //         dataSource.push(items)
+    //
+    //     })
+    // })
+
+    Promise.all(filterArr.map(u=>fetch(u))).then(responses =>
+        Promise.all(responses.map(res => res.json()))
+    )
+    .then(texts => {
+        cocktailArr(texts)
     })
+
+
+    // displayCocktails(dataSource)
+    // console.log(dataSource);
+
+    //
+    //
+    // console.log(dataSource);
+}
+
+getCocktails()
+
+function cocktailArr(cocktailArr) {
+    cocktailArr.forEach(items =>{
+        (items.drinks).forEach(item =>{
+            dataSource.push(item);
+        })
+    })
+    displayCocktails(dataSource);
 }
 
 
-getCocktails();
-
-const filteList = ['Ordinary Drink', 'Cocktail','Milk / Float / Shake','Other/Unknown','Cocoa','Shot','Coffee / Tea','Homemade Liqueur','Beer'];
-
-
-
 function displayCocktails(cocktails){
+
     let cocktailsHtml = '';
+    console.log(cocktails);
 
     for(let i = startPost; i< endPost; i++){
         cocktailsHtml += `<div class='cocktail-item'>
@@ -59,11 +101,10 @@ function displayCocktails(cocktails){
 let listItems = '';
 let filtresItems = ''
 
-filteList.forEach((item,i) =>{
-
+filterList.forEach((item,index) =>{
         listItems +=
         `<li class='filter-item'><label for=${item}>${item}</label>
-    <input value=${item} id=${item} ${i === 0 ? checked="checked" : null} type="checkbox"/></li>`
+    <input value=${index} id=${item} ${index === 0 ? checked="checked" : ''} type="checkbox"/></li>`
 });
 
 
@@ -83,12 +124,9 @@ backBtn.addEventListener('click', ()=>{
 
 window.addEventListener('scroll', () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
     console.log( { scrollTop, scrollHeight, clientHeight });
-
     if(clientHeight + scrollTop >= scrollHeight - 5) {
-
-        displayCocktails(dataSource)
+        getCocktails(dataSource)
     }
 });
 
@@ -99,12 +137,11 @@ function getCheckedCheckBoxes() {
     let selectedCheckBoxes = document.querySelectorAll('input:checked');
     let checkedValues = Array.from(selectedCheckBoxes).map(cb => cb.value);
 
-
     checkedValues.forEach(item =>{
         filtresItems += `<li>${item}</li>`
     });
     filterChekedBlock.innerHTML = filtresItems
-
+    getCocktails(checkedValues);
 }
 getCheckedCheckBoxes();
 
