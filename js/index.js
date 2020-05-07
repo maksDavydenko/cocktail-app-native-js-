4235a3f79d8fad697bfd801c8d418c73d55411e8
max davydenko <maksim.davydenko@gmail.com>
2020-05-07 13:53:38 +0300

    add reset setings

--- js/index.js
+++ js/index.js
@@ -12,64 +12,23 @@ let startPost = 0;
 let endPost = 6;
 const filterList = ['Ordinary Drink', 'Cocktail','Milk / Float / Shake','Other/Unknown','Cocoa','Shot','Coffee / Tea','Homemade Liqueur','Beer'];
 
-function getCocktails() {
+function getCocktails(filterLists) {
 
     const url ='https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
-    const filterArr = filterList.map(item => url + item);
+    const filterArr = filterLists.map(item => url + filterList[item]);
 
-    // filterArr.forEach(item => {
-    //     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filterList[4]}`)
-    // Promise.all(filterArr.map(u=>fetch(u))).then(responses =>
-    //     Promise.all(responses.map(res => res.text()))
-    // )
-    // .then(
-    //         function (response) {
-    //             if (response.status !== 200) {
-    //                 console.log(`server has some problem ${response.status}`);
-    //                 return;
-    //             }
-    //             response.json().then(function (data) {
-    //                 (data.drinks).forEach(cocktail => {
-    //                     dataSource.push(cocktail);
-    //                 })
-    //             });
-    //         }
-    //     )
-    //     .catch(function (err) {
-    //         console.log(`fetch err ${err}`);
-    //     })
-
-    // Promise.all(filterArr.map(url =>
-    //     fetch(url).then(resp => resp.json())
-    // )).then(texts => {
-    //     texts.forEach(items => {
-    //        //  (items.drinks).forEach(item => {
-    //        //     dataSource.push(item);
-    //        // })
-    //         dataSource.push(items)
-    //
-    //     })
-    // })
-
-    Promise.all(filterArr.map(u=>fetch(u))).then(responses =>
-        Promise.all(responses.map(res => res.json()))
-    )
-    .then(texts => {
-        cocktailArr(texts)
+    Promise.all(filterArr.map(url =>
+        fetch(url).then(resp => resp.json())
+    )).then(cocktails => {
+        cocktailArr(cocktails)
     })
 
-
-    // displayCocktails(dataSource)
-    // console.log(dataSource);
-
-    //
-    //
-    // console.log(dataSource);
 }
 
-getCocktails()
+// getCocktails()
 
 function cocktailArr(cocktailArr) {
+
     cocktailArr.forEach(items =>{
         (items.drinks).forEach(item =>{
             dataSource.push(item);
@@ -80,19 +39,19 @@ function cocktailArr(cocktailArr) {
 
 
 function displayCocktails(cocktails){
-
     let cocktailsHtml = '';
     console.log(cocktails);
 
     for(let i = startPost; i< endPost; i++){
         cocktailsHtml += `<div class='cocktail-item'>
-        <img width=100 height=100 src="${cocktails[i].strDrinkThumb}" alt=''/>
+        <img width=100 height=100 src="${cocktails[i].strDrinkThumb == undefined ? './img/no img.png' : cocktails[i].strDrinkThumb}" alt=''/>
          <p class='cocktail-item__text'>
          ${cocktails[i].strDrink}</p>
          </div>`
     }
 
     wrap.innerHTML += cocktailsHtml
+    filterChekedBlock.innerHTML = `result is: ${dataSource.length}`
 
     startPost += 4;
     endPost += 4;
@@ -125,8 +84,8 @@ backBtn.addEventListener('click', ()=>{
 window.addEventListener('scroll', () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     console.log( { scrollTop, scrollHeight, clientHeight });
-    if(clientHeight + scrollTop >= scrollHeight - 5) {
-        getCocktails(dataSource)
+    if(clientHeight + scrollTop >= scrollHeight - 20) {
+        displayCocktails(dataSource)
     }
 });
 
@@ -140,11 +99,16 @@ function getCheckedCheckBoxes() {
     checkedValues.forEach(item =>{
         filtresItems += `<li>${item}</li>`
     });
-    filterChekedBlock.innerHTML = filtresItems
     getCocktails(checkedValues);
+    console.log(checkedValues);
 }
 getCheckedCheckBoxes();
 
 filterApplyBtn.addEventListener('click', ()=>{
+    wrap.innerHTML = '';
     getCheckedCheckBoxes();
+    dataSource.length = 0;
+    backBtn.click();
+    startPost = 0;
+    endPost = 6;
 })
