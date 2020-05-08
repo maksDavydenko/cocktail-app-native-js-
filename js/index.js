3710ab860402bad9a006b47cf67a6ee90ae011d8
max davydenko <maksim.davydenko@gmail.com>
2020-05-08 15:42:12 +0300

    add fetch filters

--- js/index.js
+++ js/index.js
@@ -2,100 +2,119 @@ const wrap = document.querySelector('.wrap');
 const filterBlock = document.querySelector('.filtres-block');
 const filterBtn = document.querySelector('.filter-btn');
 const backBtn = document.querySelector('.back-btn');
-const cocktailsList= document.querySelector('.cocktails-list');
+const cocktailsList = document.querySelector('.cocktails-list');
 const filterApplyBtn = document.querySelector('.submit-btn');
 const filterChekedBlock = document.querySelector('.filterChekedBlock');
-const nothigErr = document.querySelector('.nothing-err')
+const nothingErr = document.querySelector('.nothing-err')
 const dataSource = [];
 
 let startPost = 0;
 let endPost = 6;
-const filterList = ['Ordinary Drink', 'Cocktail','Milk / Float / Shake','Other/Unknown','Cocoa','Shot','Coffee / Tea','Homemade Liqueur','Beer'];
+const filterList = [];
 
-function getCocktails(filterLists) {
+getFiltersItems();
 
-    const url ='https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
-    const filterArr = filterLists.map(item => url + filterList[item]);
+function getFiltersItems() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        getFilterArr(data);
+    });
+}
 
+function getFilterArr(arr){
+    (arr.drinks).forEach(item =>{
+        filterList.push(item.strCategory);
+    })
+
+    growFilterList();
+}
+
+
+
+
+function getCocktails(checkedValue) {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
+    const filterArr = checkedValue.map(item => url + filterList[item]);
     //multi fetch
+
     Promise.all(filterArr.map(url =>
         fetch(url).then(resp => resp.json())
     )).then(cocktails => {
         cocktailArr(cocktails)
     })
-
 }
 
 function cocktailArr(cocktailArr) {
-
-    cocktailArr.forEach(items =>{
-        (items.drinks).forEach(item =>{
+    cocktailArr.forEach(items => {
+        (items.drinks).forEach(item => {
             dataSource.push(item);
         })
     })
-
-    // load more data
-    displayCocktails(dataSource);
+    displayCocktails(dataSource)
 }
 
 
-function displayCocktails(cocktails){
+function displayCocktails(cocktails) {
     let cocktailsHtml = '';
 
-    if(cocktails.length === 0){
-        nothigErr.style.display = 'block';
-    }
-    else{
-        for(let i = startPost; i< endPost; i++){
+    if (cocktails.length === 0) {
+        nothingErr.style.display = 'block';
+    } else {
+        for (let i = startPost; i < endPost; i++) {
             cocktailsHtml += `<div class='cocktail-item'>
-        <img width=100 height=100 src="${cocktails[i].strDrinkThumb == undefined ? './img/no img.png' : cocktails[i].strDrinkThumb}" alt=''/>
+        <img width=100 height=100 src="${cocktails[i].strDrinkThumb == undefined ? './img/no img.png' : cocktails[i].strDrinkThumb}" alt="${cocktails[i].strDrink}"/>
          <p class='cocktail-item__text'>
          ${cocktails[i].strDrink}</p>
          </div>`
         }
         startPost += 4;
         endPost += 4;
-        nothigErr.style.display = 'none';
+        nothingErr.style.display = 'none';
     }
 
     wrap.innerHTML += cocktailsHtml
     filterChekedBlock.innerHTML = `Result is: ${dataSource.length} cocktails`
 
 }
-let listItems = '';
-let filtresItems = ''
 
-filterList.forEach((item,index) =>{
+
+
+function growFilterList() {
+    let listItems = '';
+
+    filterList.forEach((item, index) => {
         listItems +=
-        `<li class='filter-item'><label for=${item}>${item}</label>
-    <input value=${index} id=${item} ${index === 0 ? checked="checked" : ''} type="checkbox"/></li>`
-});
-
-
-
-cocktailsList.innerHTML = listItems;
+            `<li class='filter-item'><label for=${item}>${item}</label>
+    <input value=${index} id=${item} ${index === 0 ? checked = 'checked' : ''} type="checkbox"/></li>`
+    });
+    cocktailsList.innerHTML = listItems;
+    getCheckedCheckBoxes();
+    displayCocktails(dataSource);
+}
 
 // toggle menu
-filterBtn.addEventListener('click', ()=>{
-    filterBlock.style.left = '0px'
-    nothigErr.style.display = 'none';
-    document.body.style.overflowY = 'hidden'
 
+filterBtn.addEventListener('click', () => {
+    filterBlock.style.left = '0px'
+    nothingErr.style.display = 'none';
+    document.body.style.overflowY = 'hidden'
+    popUp.style.display = 'none'
 })
 
-backBtn.addEventListener('click', ()=>{
+backBtn.addEventListener('click', () => {
     filterBlock.style.left = '999px';
     document.body.style.overflowY = 'scroll'
-
-
 })
 
 //infinity scroll
 
 window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    console.log( { scrollTop, scrollHeight, clientHeight });
-    if(clientHeight + scrollTop >= scrollHeight - 20) {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    console.log({scrollTop, scrollHeight, clientHeight});
+    if (clientHeight + scrollTop >= scrollHeight - 20) {
         displayCocktails(dataSource)
     }
 });
@@ -103,19 +122,19 @@ window.addEventListener('scroll', () => {
 // select checked value
 
 function getCheckedCheckBoxes() {
+    let filtresItems = ''
     let selectedCheckBoxes = document.querySelectorAll('input:checked');
     let checkedValues = Array.from(selectedCheckBoxes).map(cb => cb.value);
 
-    checkedValues.forEach(item =>{
+    checkedValues.forEach(item => {
         filtresItems += `<li>${item}</li>`
     });
 
     getCocktails(checkedValues);
 }
 
-getCheckedCheckBoxes();
 
-filterApplyBtn.addEventListener('click', ()=>{
+filterApplyBtn.addEventListener('click', () => {
     wrap.innerHTML = '';
     getCheckedCheckBoxes();
     dataSource.length = 0;
@@ -124,3 +143,33 @@ filterApplyBtn.addEventListener('click', ()=>{
     startPost = 0;
     endPost = 6;
 })
+
+
+// wrap click
+const popUp = document.querySelector('.popUp');
+let closeBtn = null;
+
+
+wrap.addEventListener('click', () => {
+    let target = event.target;
+
+    if (target.closest('div').classList.contains('cocktail-item')) {
+        let block = target.closest('div')
+        let outers = block.childNodes;
+        let filterItem;
+        outers.forEach((item, i) => {
+            (outers[i].tagName === 'IMG') ? filterItem = outers[i] : null;
+        })
+
+        popUp.innerHTML = `<img src="${filterItem.src}" width="350" height="350" alt="${filterItem.alt}">
+<p>${filterItem.alt}</p>
+        <div class="close"><img src="./img/close-icon.svg" width="20" height="20" alt="x"></div>`
+        popUp.style.display = 'block'
+        closeBtn = document.querySelector('.close');
+        console.log(closeBtn);
+    }
+
+    console.log(target.closest('div').classList.contains('testClose'));
+})
+
+
